4983ae9b1a920da31ba4e3ccf6de6f563caa7403
albert <albert3787@gmail.com>
2022-08-04 19:18:11 +0800

    Combine firstname and lastname column,Update Homepage content by check if user or not, Add nickname column to user table

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
-  # get 'home/index'
+  resources :friends
+
   root 'home#index'
   get 'home/about'
-  resources :friends
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_07_20_111525) do
+ActiveRecord::Schema.define(version: 2022_08_04_101804) do
 
   create_table "friends", force: :cascade do |t|
     t.string "first_name"
@@ -32,6 +32,7 @@ ActiveRecord::Schema.define(version: 2022_07_20_111525) do
     t.datetime "remember_created_at"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "nickname"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
